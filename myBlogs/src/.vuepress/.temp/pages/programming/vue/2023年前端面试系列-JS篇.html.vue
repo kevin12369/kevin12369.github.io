28771e16c70a0454b50ce8232fb17a1d0f3d845a
Kevin0416 <491750329@qq.com>
2023-03-31 18:02:11 +0800

    2023-03-31 更新AST抽象语法树材料

--- myBlogs/src/.vuepress/.temp/pages/programming/vue/2023年前端面试系列-JS篇.html.vue
+++ myBlogs/src/.vuepress/.temp/pages/programming/vue/2023年前端面试系列-JS篇.html.vue
@@ -98,6 +98,84 @@ Array<span class="token punctuation">.</span><span class="token function">isArra
 </li>
 </ul>
 <h2 id="如何遍历对象的属性" tabindex="-1"><a class="header-anchor" href="#如何遍历对象的属性" aria-hidden="true">#</a> 如何遍历对象的属性？</h2>
+<ul>
+<li>遍历自身 <strong>可枚举的属性（可枚举，非继承属性）</strong>：<code v-pre>Object.keys()</code>方法，该方法会返回一个由给定对象的自身可枚举属性组成的数组。</li>
+<li>遍历自身的 <strong>所有属性（可枚举、不可枚举、非集成属性）</strong>：<code v-pre>Object.getOwnPropertyNames()</code>方法，该方法会返回一个由指定对象的所有自身属性组成的数组。</li>
+<li>遍历 <strong>可枚举的自身属性和继承属性</strong>：<code v-pre>for ... in ...</code></li>
+</ul>
+<h2 id="如何判断两个对象是否相等" tabindex="-1"><a class="header-anchor" href="#如何判断两个对象是否相等" aria-hidden="true">#</a> 如何判断两个对象是否相等？</h2>
+<ol>
+<li>
+<p>Object,is(obj1,obj2),判断两个对象都引用地址是否一致，true 则一致，false 则不一致。</p>
+</li>
+<li>
+<p>判断两个对象内容是否一致，思路是遍历对象的所有键名和键值是否都一致</p>
+<p>① 判断两个对象是都指向同一内存</p>
+<p>② 使用<code v-pre>Object.getOwnPropertyNames</code>获取对象所有键名数组</p>
+<p>③ 判断两个对象的键名数组是否相等</p>
+<p>④ 遍历键名，判断键值是否都相等</p>
+</li>
+</ol>
+<div class="language-javascript line-numbers-mode" data-ext="js"><pre v-pre class="language-javascript"><code><span class="token comment">// 函数实现</span>
+<span class="token keyword">function</span> <span class="token function">isObjectValueEqual</span><span class="token punctuation">(</span><span class="token parameter">a<span class="token punctuation">,</span> b</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+  <span class="token comment">// 判断两个对象是否指向同一内存，若是则返回true</span>
+  <span class="token keyword">if</span> <span class="token punctuation">(</span>a <span class="token operator">===</span> b<span class="token punctuation">)</span> <span class="token keyword">return</span> <span class="token boolean">true</span>
+  <span class="token comment">// 获取两个对象的键名数组</span>
+  <span class="token keyword">let</span> aProps <span class="token operator">=</span> Object<span class="token punctuation">.</span><span class="token function">getOwnPropertyNames</span><span class="token punctuation">(</span>a<span class="token punctuation">)</span>
+  <span class="token keyword">let</span> bProps <span class="token operator">=</span> Object<span class="token punctuation">.</span><span class="token function">getOwnPropertyNames</span><span class="token punctuation">(</span>b<span class="token punctuation">)</span>
+  <span class="token comment">// 判断两个键名数组的长度是否一致，不一致则返回false</span>
+  <span class="token keyword">if</span> <span class="token punctuation">(</span>aProps<span class="token punctuation">.</span>length <span class="token operator">!==</span> bProps<span class="token punctuation">.</span>length<span class="token punctuation">)</span> <span class="token keyword">return</span> <span class="token boolean">false</span>
+  <span class="token comment">// 遍历对象的键值</span>
+  <span class="token keyword">for</span> <span class="token punctuation">(</span><span class="token keyword">let</span> prop <span class="token keyword">in</span> a<span class="token punctuation">)</span> <span class="token punctuation">{</span>
+    <span class="token comment">// 判断 a 的键名，在 b 中是否存在，若不存在则返回false</span>
+    <span class="token keyword">if</span> <span class="token punctuation">(</span>b<span class="token punctuation">.</span><span class="token function">hasOwnProperty</span><span class="token punctuation">(</span>prop<span class="token punctuation">)</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+      <span class="token comment">// 判断 a 的键值是否为对象，若是则需要递归</span>
+      <span class="token comment">// 若不是，直接判断键值是否相等，若不等则返回false</span>
+      <span class="token keyword">if</span> <span class="token punctuation">(</span><span class="token keyword">typeof</span> a<span class="token punctuation">[</span>prop<span class="token punctuation">]</span> <span class="token operator">===</span> <span class="token string">'object'</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+        <span class="token keyword">if</span> <span class="token punctuation">(</span><span class="token operator">!</span><span class="token function">isObjectValueEqual</span><span class="token punctuation">(</span>a<span class="token punctuation">[</span>prop<span class="token punctuation">]</span><span class="token punctuation">,</span> b<span class="token punctuation">[</span>prop<span class="token punctuation">]</span><span class="token punctuation">)</span><span class="token punctuation">)</span> <span class="token keyword">return</span> <span class="token boolean">false</span>
+      <span class="token punctuation">}</span> <span class="token keyword">else</span> <span class="token keyword">if</span> <span class="token punctuation">(</span>a<span class="token punctuation">[</span>prop<span class="token punctuation">]</span> <span class="token operator">!==</span> b<span class="token punctuation">[</span>prop<span class="token punctuation">]</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+        <span class="token keyword">return</span> <span class="token boolean">false</span>
+      <span class="token punctuation">}</span>
+    <span class="token punctuation">}</span> <span class="token keyword">else</span> <span class="token punctuation">{</span>
+      <span class="token keyword">return</span> <span class="token boolean">false</span>
+    <span class="token punctuation">}</span>
+  <span class="token punctuation">}</span>
+  <span class="token keyword">return</span> <span class="token boolean">true</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="强制类型转换和隐式类型转换有哪些" tabindex="-1"><a class="header-anchor" href="#强制类型转换和隐式类型转换有哪些" aria-hidden="true">#</a> 强制类型转换和隐式类型转换有哪些？</h2>
+<ul>
+<li>
+<p>强制：</p>
+<p>转换成字符串：toString(),String()</p>
+<p>转换成数字：Number(),parseInt(),parseFloat()</p>
+<p>转换成布尔类型：Boolean()</p>
+</li>
+<li>
+<p>隐式：</p>
+<p>拼接字符串：let str = 1 + &quot;&quot;;</p>
+</li>
+</ul>
+<h2 id="js-中的作用于和作用域链" tabindex="-1"><a class="header-anchor" href="#js-中的作用于和作用域链" aria-hidden="true">#</a> JS 中的作用于和作用域链？</h2>
+<p><code v-pre>作用域</code>，即变量（变量作用域又称上下文）和函数生效（能被访问）的区域或集合。作用域决定了代码区块中变量和其他资源的可见性。一般分为：<code v-pre>全局作用域</code>、<code v-pre>局部作用域（函数作用域）</code>、<code v-pre>块级作用域</code>。</p>
+<ul>
+<li>全局作用域：任何不在函数中或者是大括号中声明的变量，都是在全局作用域下，全局作用域下声明的变量可以在程的任意位置访问。</li>
+<li>局部作用域：也叫做函数作用域，如果一个变量是在函数内部声明的，它就在一个函数作用域下面。这些变量只能在函数内部访问，不能再函数以外去访问。</li>
+<li>块级作用域：凡是代码块就可以划分变量的作用域，这种作用域的规则就叫做块级作用域。</li>
+</ul>
+<p>作用域链：当在 JS 中使用一个变量时，JS 引擎会尝试在当前作用域下寻找该变量，如果没找到，再到它的上层作用域寻找，以此类推，直至找到该变量或是查找至全局作用域，如果在全局作用域里仍然找不到该变量，它就会在全局范围内隐式声明该变量（非严格模式下）或是直接报错。</p>
+<p><strong>js 采用的是静态作用域，所以函数的作用域在函数定义时就确定了</strong></p>
+<p>推荐阅读：<a href="https://github.com/mqyqingfeng/Blog/issues/3" target="_blank" rel="noopener noreferrer">JavaScript 深入之词法作用域和动态作用域<ExternalLinkIcon/></a></p>
+<h2 id="js-的预解析" tabindex="-1"><a class="header-anchor" href="#js-的预解析" aria-hidden="true">#</a> JS 的预解析</h2>
+<p>JS 代码的执行是由浏览器中的 JS 解析器来执行的，JS 解析器执行 JS 代码时，分为两个过程：<code v-pre>预解析过程</code>和<code v-pre>代码执行过程</code>。预解析分为<code v-pre>变量预解析（变量提升）</code>和<code v-pre>函数预解析（函数提升</code>）；代码执行是指按顺序从上至下执行：</p>
+<ul>
+<li>变量提升：把变量的声明提升到当前作用域的最前面，只提升声明，不提升赋值；</li>
+<li>函数提升：把函数的声明提升到当前作用域的最前面，只提升声明，不提升调用；</li>
+</ul>
+<p><strong>函数表达式的写法不存在函数提升</strong></p>
+<p><strong>函数提升优先级高于变量提升，即函数提升在变量提升之上，且不会被同名变量声明时覆盖，但是会被同名变量赋值后覆盖</strong></p>
+<h2 id="array-from-和-array-of-的使用及区别" tabindex="-1"><a class="header-anchor" href="#array-from-和-array-of-的使用及区别" aria-hidden="true">#</a> Array.from()和 Array.of()的使用及区别？</h2>
+<p><code v-pre>Array.from()</code>：将伪数组对象或可遍历对象转换为真数组。接受三个参数：input、map、context。input：待转换的伪数组对象或可遍历对象；map：类似于数组的 map 方法，用来对每个元素进行处理，将处理后的值放入返回的数组；context：绑定 map 中用到的 this。</p>
+<p><code v-pre>Array.of()</code>：将一系列值转换成数组，会创建一个包含所有传入参数的数组，而不管参数的数量与类型，解决了 <code v-pre>new Array()</code>行为不统一的问题。</p>
 </div></template>
 
 
