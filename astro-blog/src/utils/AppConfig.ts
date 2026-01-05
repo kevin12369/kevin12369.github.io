9ccd882b692216f5688cd40cef446f6453471cc7
kevin12369 <491750329@qq.com>
2026-01-05 19:58:48 +0800

    Replace old blog with new theme and reorganized content

--- astro-blog/src/utils/AppConfig.ts
+++
@@ -1,8 +0,0 @@
-export const AppConfig = {
-	site_name: 'Kevin.AI',
-	title: 'Kevin.AI | 探索AI增强型全栈开发',
-	description: '探索AI增强型全栈开发的实践与思考',
-	author: 'Kevin',
-	locale_region: 'zh-CN',
-	locale: 'zh'
-};
