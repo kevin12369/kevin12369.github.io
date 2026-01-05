d13f5fdb6ed15aab2b16649e1deac894b7184227
kevin12369 <491750329@qq.com>
2026-01-05 23:51:30 +0800

    将博客标题修改为Kevin.AI

--- astro-blog-new/src/site.config.ts
+++ astro-blog-new/src/site.config.ts
@@ -3,7 +3,7 @@ import type { CardListData, Config, IntegrationUserConfig, ThemeUserConfig } fro
 export const theme: ThemeUserConfig = {
   // [Basic]
   /** Title for your website. Will be used in metadata and as browser tab title. */
-  title: 'Kevin 的技术博客',
+  title: 'Kevin.AI',
   /** Will be used in index page & copyright declaration */
   author: 'Kevin',
   /** Description metadata for your website. Can be used in page metadata. */
